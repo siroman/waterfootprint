9fdc47da9e2d9617609fdd6e817b6f8787467986
siroman <sim.romano@gmail.com>
2025-02-02 19:23:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function showForm(formId) {
+  document.getElementById(formId).style.display = 'block';
+}
+function hideForm(formId) {
+  document.getElementById(formId).style.display = 'none';
+}
+
+function showMessage() {
+  let doccia = document.getElementById('doccia').value;
+  let famiglia = document.getElementById('famiglia').value;
+  let docce = document.getElementById('docce').value;
+
+  let risultato = doccia * famiglia * docce;
+  alert('La tua impronta idrica è: ' + risultato);
+}
+function openPopup() {
+  document.getElementById('popup').style.display = 'block';
+}
+
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
+}
+
+window.onclick = function (event) {
+  if (event.target == document.getElementById('popup')) {
+    closePopup();
+  }
+};
